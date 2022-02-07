a2030651e2d0b937aea7d6f7480a022d02368908
5undel <max.sundel@Choice.se>
2022-02-07 19:38:54 +0000

    Added stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,21 @@
+var stripe_public:key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret:key = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var card = elements.create('card',  {style: style});
+
+var style = {
+    color: '#000';
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    '::placeholder': {
+        color: '#aab7c4'
+    }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form' : order_form,
+        'stripe_public_key' : 'pk_test_51KQcXaCY8qeh0zkCQYM7m8mm0nKlvCZ52fsAzxn6YJB6xr5V3O3vzAnZb4tdXBkH9sK22T06sFFzvjm7eTbsBgN1005hbRsGDk',
+        
     }
 
     return render(request, template, context)
